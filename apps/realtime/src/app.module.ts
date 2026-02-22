6817cb1c0d04e445c3ce4de625e9abd11a36ef65
Saubh <saubhtech@gmail.com>
2026-02-22 06:05:54 +0530

    feat(realtime): register CrmModule in AppModule

--- apps/realtime/src/app.module.ts
+++ apps/realtime/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
 import { ChatModule } from './chat/chat.module';
 import { RedisModule } from './redis/redis.module';
+import { CrmModule } from './crm/crm.module';
 
 @Module({
-  imports: [HealthModule, ChatModule, RedisModule],
+  imports: [HealthModule, ChatModule, RedisModule, CrmModule],
 })
 export class AppModule {}
