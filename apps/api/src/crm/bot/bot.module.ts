2193cfe0ce3399b754088a27afdb681cda6ef0b1
Saubh <saubhtech@gmail.com>
2026-02-22 05:41:13 +0530

    feat(crm): register BotController in BotModule

--- apps/api/src/crm/bot/bot.module.ts
+++ apps/api/src/crm/bot/bot.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
+import { BotController } from './bot.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { ChannelModule } from '../channels/channel.module';
 
 @Module({
   imports: [PrismaModule, ChannelModule],
+  controllers: [BotController],
   providers: [BotService],
   exports: [BotService],
 })
